da6e2488d7cc99ad3b0826354c4883eb82f01132
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-28 17:33:14 +0530

    sync

--- source/preprocess.py
+++ source/preprocess.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
 
+# Load the training, test, and sample submission data
+train_data_path = '/Users/paramanandbhat/Downloads/train_XnW6LSF.csv'
+test_data_path = '/Users/paramanandbhat/Downloads/test_FewQE9B.csv'
+sample_submission_path = '/Users/paramanandbhat/Downloads/sample_submission_hP4II7x.csv'
+
+
 
 
